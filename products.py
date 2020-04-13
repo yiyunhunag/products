362c501c82117cabcfcc29307623d496c9afef57
yiyunhuang <a1l2l0y1@gmail.com>
2020-04-13 23:47:22 +0800

    4

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#read
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)		
+
+#input
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -9,6 +19,7 @@ while True:
 	products.append(p)
 print(products)
 
+#print
 for p in products:
 	print(p[0], '的價格是', p[1])
 
